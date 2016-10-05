7d2d9bacdd4f6b37fe60d6c757fa6b4aae758130
spinalwiz <spinalwiz@gmail.com>
2016-10-05 22:08:18 +0100

    empty message

--- src/app/services/gamemodes.service.ts
+++ src/app/services/gamemodes.service.ts
@@ -5,8 +5,8 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GameModesService {
-  // API_URL: string = 'https://aimtrain.herokuapp.com';
-  API_URL: string = 'http://localhost:3000';
+  API_URL: string = 'https://aimtrain.herokuapp.com';
+  // API_URL: string = 'http://localhost:3000';
 
   private headers = new Headers({'Content-Type': 'application/json', 'charset': 'UTF-8'});
   private options = new RequestOptions({headers: this.headers});
