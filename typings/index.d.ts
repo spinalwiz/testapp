406740be5d55813970720f42063d1099d6f76f66
spinalwiz <spinalwiz@gmail.com>
2016-10-05 22:00:38 +0100

    empty message

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/mongoose/index.d.ts" />
+/// <reference path="modules/serve-static/index.d.ts" />
