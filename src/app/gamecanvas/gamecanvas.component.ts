2dc8cf249457cbacdfa25c14177d0fd916c8ba01
spinalwiz <spinalwiz@gmail.com>
2016-10-07 15:37:05 +0100

    empty message

--- src/app/gamecanvas/gamecanvas.component.ts
+++ src/app/gamecanvas/gamecanvas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'gamecanvas',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameCanvasComponent implements OnInit {
 
-  constructor() { }
+  constructor(authService: AuthService) {
+  }
 
   ngOnInit() {
+    if (!this.authService.authenticated()) {
+      this.authService.login();
+    } else {
+      this.sendAuthToken();
+    }
+  }
+
+  sendAuthToken() {
+    var win = document.getElementById("gamewindow").contentWindow;
+    win.postMessage(localStorage.getItem('id_token'));
+    //, "http://a.JavaScript.info" target domain
   }
 
 }
